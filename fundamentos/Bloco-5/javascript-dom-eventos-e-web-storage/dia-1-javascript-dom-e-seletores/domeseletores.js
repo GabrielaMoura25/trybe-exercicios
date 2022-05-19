6b45aa8980f58cd6a52a329e88c0ff98a32b1c11
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-05-19 16:00:27 -0300

    recuperei o segundo paragrafo e o alterei

--- fundamentos/Bloco-5/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/domeseletores.js
+++ fundamentos/Bloco-5/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/domeseletores.js
@@ -1,3 +1,4 @@
 const paragraph = document.getElementById("paragraph");
       paragraph.style.color = "red";
-document.getElementById("page-title").innerText = "Sempre ao seu lado"
+document.getElementById("page-title").innerText = "Sempre ao seu lado";
+document.getElementById("second-paragraph").innerText = "Posso assistir esse filme 10 vezes, que continuarei chorando nas 10"
