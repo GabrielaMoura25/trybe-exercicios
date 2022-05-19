42e25fc50e9ecfeadba07d68b5a8440707fd0023
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-05-19 18:14:04 -0300

    recuperei os paragrafos via js usando getElementsByClassName e alterei o estilo do primeiro deles

--- fundamentos/Bloco-5/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/domeseletores.js
+++ fundamentos/Bloco-5/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/domeseletores.js
@@ -3,3 +3,6 @@ const paragraph = document.getElementById("paragraph");
 document.getElementById("page-title").innerText = "Sempre ao seu lado";
 document.getElementById("second-paragraph").innerText = "Posso assistir esse filme 10 vezes, que continuarei chorando nas 10."
 document.getElementById("subtitle").innerText = "Comentários sobre o filme:"
+
+let paragrafos = document.getElementsByClassName('paragrafo');
+paragrafos[0].style.color = 'blue';
