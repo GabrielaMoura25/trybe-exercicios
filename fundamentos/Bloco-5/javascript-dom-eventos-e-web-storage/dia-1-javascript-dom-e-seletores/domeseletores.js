423b68b177f73d8f61626600d4556a2580b7145d
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-05-19 16:02:07 -0300

    recuperei o sutítulo e o alterei

--- fundamentos/Bloco-5/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/domeseletores.js
+++ fundamentos/Bloco-5/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/domeseletores.js
@@ -1,4 +1,5 @@
 const paragraph = document.getElementById("paragraph");
       paragraph.style.color = "red";
 document.getElementById("page-title").innerText = "Sempre ao seu lado";
-document.getElementById("second-paragraph").innerText = "Posso assistir esse filme 10 vezes, que continuarei chorando nas 10"
+document.getElementById("second-paragraph").innerText = "Posso assistir esse filme 10 vezes, que continuarei chorando nas 10."
+document.getElementById("subtitle").innerText = "Comentários sobre o filme:"
